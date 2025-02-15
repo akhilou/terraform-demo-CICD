38ee628948c3b26a0ae7ac43c55f116929a6b7ea
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-15 19:48:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,10 +37,19 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_igw"
+  }
+}
+
 resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr1
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr1
   availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "subnet1"
@@ -48,13 +57,37 @@ resource "aws_subnet" "subnet1" {
 }
 
 resource "aws_subnet" "subnet2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr2
   availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "subnet2"
   }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "main_rt"
+  }
+}
+
+resource "aws_route_table_association" "subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.main.id
 }
 
 resource "aws_security_group" "web_sg" {
@@ -85,18 +118,16 @@ resource "aws_security_group" "web_sg" {
     Name = "web_sg"
   }
 }
-
 resource "aws_instance" "web" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.subnet1.id
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  ami                  = var.ami
+  instance_type        = var.instance_type
+  subnet_id            = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "web_instance"
   }
 }
-
 resource "aws_lb" "alb" {
   name               = "app-lb"
   internal           = false
