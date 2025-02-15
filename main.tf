4be5d5ade931d5e1444de1362b3c59c8d0b01b84
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-15 19:39:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,12 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "subnet_cidr" {
+variable "subnet_cidr1" {
   default = "10.0.1.0/24"
+}
+
+variable "subnet_cidr2" {
+  default = "10.0.2.0/24"
 }
 
 variable "instance_type" {
@@ -33,15 +37,26 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidr
-  map_public_ip_on_launch = true
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr1
+  availability_zone = "ap-south-1a"
 
   tags = {
-    Name = "main_subnet"
+    Name = "subnet1"
   }
 }
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr2
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
 resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.main.id
 
@@ -70,23 +85,24 @@ resource "aws_security_group" "web_sg" {
     Name = "web_sg"
   }
 }
+
 resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
-  key_name = "dockernew"
-  security_groups = [aws_security_group.web_sg.name]
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.subnet1.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "web_instance"
   }
 }
+
 resource "aws_lb" "alb" {
   name               = "app-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web_sg.id]
-  subnets            = [aws_subnet.main.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 
   tags = {
     Name = "app-lb"
@@ -125,3 +141,4 @@ resource "aws_lb_target_group_attachment" "web_instance" {
   target_id        = aws_instance.web.id
   port             = 80
 }
+
