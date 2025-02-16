072646c2bc323fdf1bb5fab833bc45a616f0d2cd
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-16 18:05:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,42 +9,6 @@ terraform {
     region = "ap-south-1"
   }
 }
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "subnet_cidr1" {
-  default = "10.0.1.0/24"
-}
-
-variable "subnet_cidr2" {
-  default = "10.0.2.0/24"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "ami" {
-  default = "ami-00bb6a80f01f03502"
-}
-
-variable "domain_name" {
-  description = "cloudwithsunil.xyz"
-  default     = "cloudwithsunil.xyz"
-}
-
-variable "subdomain_name" {
-  description = "www.cloudwithsunil.xyz"
-  default     = "www.cloudwithsunil.xyz"
-}
-
-variable "certificate_arn" {
-  description = "ARN of the ACM certificate for HTTPS listener"
-  default     = "arn:aws:acm:ap-south-1:007903962438:certificate/cc05b44c-fd8c-4a34-ab2f-8ff966dccac4"
-}
-
 module "vpc" {
   source    = "./modules/vpc"
   vpc_cidr  = var.vpc_cidr
