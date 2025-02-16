befc2a2da96d5ca40be8aba16ec63532354ddc7e
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-16 18:01:54 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,1 +1,25 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "subnet1_id" {
+  description = "The ID of subnet 1"
+  type        = string
+}
+
+variable "subnet2_id" {
+  description = "The ID of subnet 2"
+  type        = string
+}
+
+variable "alb_sg_id" {
+  description = "The ID of the ALB security group"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "The ARN of the ACM certificate for HTTPS listener"
+  type        = string
+}
 
