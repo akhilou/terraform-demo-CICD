29aaf494c066196bec11f6c1e66f9839d95af476
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-16 17:56:05 +0530

    Update variables.tf

--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -1,1 +1,30 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "route_table_id" {
+  description = "The ID of the route table"
+  type        = string
+}
+
+variable "subnet_cidr1" {
+  description = "The CIDR block for subnet 1"
+  type        = string
+}
+
+variable "subnet_cidr2" {
+  description = "The CIDR block for subnet 2"
+  type        = string
+}
+
+variable "availability_zone1" {
+  description = "The availability zone for subnet 1"
+  type        = string
+}
+
+variable "availability_zone2" {
+  description = "The availability zone for subnet 2"
+  type        = string
+}
 
