81b07616192c507530b3fc771623d0575e61d275
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-16 18:02:57 +0530

    Update variables.tf

--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -1,1 +1,20 @@
+variable "domain_name" {
+  description = "The domain name"
+  type        = string
+}
+
+variable "subdomain_name" {
+  description = "The subdomain name"
+  type        = string
+}
+
+variable "alb_dns_name" {
+  description = "The DNS name of the ALB"
+  type        = string
+}
+
+variable "alb_zone_id" {
+  description = "The Zone ID of the ALB"
+  type        = string
+}
 
