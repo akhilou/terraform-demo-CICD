0b98e1bd8df3ba9e08934d0d1fb8cd8954bad966
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-16 17:58:09 +0530

    Update varaibles.tf

--- modules/ec2/varaibles.tf
+++ modules/ec2/varaibles.tf
@@ -1,1 +1,20 @@
+variable "ami" {
+  description = "The AMI ID for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet"
+  type        = string
+}
+
+variable "web_sg_id" {
+  description = "The ID of the web security group"
+  type        = string
+}
 
