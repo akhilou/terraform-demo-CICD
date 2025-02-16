950d03518528129a987f29552782f1fb876d8471
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-16 17:55:42 +0530

    Update main.tf

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -1,1 +1,32 @@
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_cidr1
+  availability_zone       = var.availability_zone1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_cidr2
+  availability_zone       = var.availability_zone2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
+resource "aws_route_table_association" "subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = var.route_table_id
+}
+
+resource "aws_route_table_association" "subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = var.route_table_id
+}
 
