88a1de3e0ac96d16c0aa99a7ccb1a9a1b788d7aa
sunil-challagali <108851025+sunil-challagali@users.noreply.github.com>
2025-02-16 17:57:49 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,20 @@
+resource "aws_instance" "web" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.web_sg_id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y nginx
+              sudo sed -i 's/<h1>Welcome to nginx!<\/h1>/<h1>Welcome to Terraform<\/h1>/' /var/www/html/index.nginx-debian.html
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "web_instance"
+  }
+}
 
