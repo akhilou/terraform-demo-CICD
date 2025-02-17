b6372edc0686c0b2496cbc0777ed020cf93d3963
akhilou <akhilmyaka99@gmail.com>
2025-02-17 12:32:29 +0530

    Update main.tf

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -2,15 +2,4 @@ data "aws_route53_zone" "main" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "app_record" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = var.subdomain_name
-  type    = "A"
-  alias {
-    name                   = var.alb_dns_name
-    zone_id                = var.alb_hosted_zone_id
-    evaluate_target_health = true
-  }
-}
-
 
