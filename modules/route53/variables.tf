4f099dfb5732d7fd09f53ba274b23b43f1607589
akhilou <akhilmyaka99@gmail.com>
2025-02-17 12:33:21 +0530

    Update variables.tf

--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -1,20 +1,5 @@
-variable "domain_name" {
-  description = "The domain name"
-  type        = string
-}
-
-variable "subdomain_name" {
-  description = "The subdomain name"
-  type        = string
-}
-
 variable "alb_dns_name" {
   description = "The DNS name of the ALB"
-  type        = string
-}
-
-variable "alb_hosted_zone_id" {
-  description = "The hosted zone ID of the ALB"
   type        = string
 }
 
