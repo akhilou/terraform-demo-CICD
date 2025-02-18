c84fea927da12bd9e425090be3024318893a4642
akhilou <akhilmyaka99@gmail.com>
2025-02-18 11:49:15 +0530

    Update main.tf

--- setup-backend/main.tf
+++ setup-backend/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "this" {
   bucket = "my-terraform-state-bucket"
-  aws_s3_bucket_acl    = "public"
+  acl    = "public"
 
   tags = {
     Name = "my-terraform-state-bucket"
