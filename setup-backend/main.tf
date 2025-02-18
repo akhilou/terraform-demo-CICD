be81e2982803ad085fd2351b02c11884765bfde7
akhilou <akhilmyaka99@gmail.com>
2025-02-18 11:35:35 +0530

    Create main.tf

---
+++ setup-backend/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "my-terraform-state-bucket"
+  acl    = "private"
+
+  tags = {
+    Name = "my-terraform-state-bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "folder" {
+  bucket = aws_s3_bucket.this.bucket
+  key    = "terraform-state-file/"
+  content = ""
+}
