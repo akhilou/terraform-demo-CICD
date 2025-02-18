fa07c09e4b2d5500e45ab08757764f00aa0ffa58
akhilou <akhilmyaka99@gmail.com>
2025-02-18 11:24:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,9 @@ variable "instance_type" {
 variable "ami" {
   default = "ami-00bb6a80f01f03502"
 }
+
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "my-terraform-s3-bucket"
+}
